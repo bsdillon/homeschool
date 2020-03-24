f47934fe2a5d1f2192f65f51a18c58c961994053
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-03-24 07:45:21 -0400

    Update animals.js

--- animals.js
+++ animals.js
@@ -240,7 +240,7 @@ function Multi()
 {
   this.Type="Mulit";
   this.chromosomes=[];
-  this.sets = 10;
+  this.sets = 20;
   this.paired = true;
   createChromosomes(this);
 }
@@ -272,7 +272,7 @@ function Slicer()
 {
   this.Type="Slicer";
   this.chromosomes=[];
-  this.sets = 10;
+  this.sets = 20;
   this.paired = true;
   createChromosomes(this);
 }
