1871b7f7f8d85e672e9471bbd61d1e002884b867
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-07-12 07:51:31 -0400

    Update student.js

--- Math/student.js
+++ Math/student.js
@@ -123,7 +123,7 @@ function HannaMath()
 {
   var c = new LinearEquations();
   var s = "<table>"
-  s+="<tr><td>Graph the circle.<br>"+Circle()+"</td></tr>";
+  s+="<tr><td>Graph the circle.<br>"+circle()+"</td></tr>";
   for(let i=0; i<3; i++)
   {
     s+="<tr>";
