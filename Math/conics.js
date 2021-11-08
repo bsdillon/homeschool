f82d362fbdb0326b16d8950c51c061f6b07ded33
bsdillon <57143227+bsdillon@users.noreply.github.com>
2021-11-08 17:17:40 -0500

    New answers for math
    Added answer for all conic sections

--- Math/conics.js
+++ Math/conics.js
@@ -3,13 +3,28 @@ if(typeof MathDefined === "undefined")
   alert("conics.js requires math.js");
 }
 
+var relations = ["=","&lt;","&gt;","&ge;","&le;"];
+
+function getRelationshipNumber()
+{
+  return Math.floor(Random()*5);
+}
+
 function line()
 {
   a = flip(Math.floor(Random()*9+1));
   b = flip(Math.floor(Random()*9+1));
   c = flip(Math.floor(Random()*5+1))*b;
 
-  return a+"x + "+b+"y "+ relation()+" "+c;
+  var temp = a+"x + "+b+"y "+ relation()+" "+c;
+
+  var answer = temp+"<br><div class='answer' name='answer'>";
+  answer +=a+"x + "+b+"(0)="+c+"&nbsp;&nbsp;Test for y=0<br>";
+  answer +="x="+c+"/"+a+"<br><br>";
+  answer +=a+"{0} + "+b+"x="+c+"&nbsp;&nbsp;Test for x=0<br>";
+  answer +="y="+c+"/"+b+"<div>";
+
+  return answer;
 }
 
 function parabola()
@@ -22,36 +37,71 @@ function parabola()
   if(Random()>.5)
   {
     a=-a;
+  }
+
+  if(Random()>.5)
+  {
     b=-b;
   }
 
   if(Random()>.5)
   {
-    var tmp = "y "+ relation() + "("+a+"x ";
-    if(Random()>.5)
-    {
-      tmp += "+" + Math.abs(b) + ")("+c+"x ";
-    }
-    else
-    {
-      tmp += "&minus;" + Math.abs(b) + ")("+c+"x ";
-    }
+    c=-c;
+  }
 
-    if(Random()>.5)
-    {
-      tmp += "+" + Math.abs(d) + ")";
-    }
-    else
-    {
-      tmp += "&minus;" + Math.abs(d) + ")";
-    }
-    
-    return tmp;
+  if(Random()>.5)
+  {
+    d=-d;
+  }
+
+  var rel = getRelationshipNumber();
+
+  var form1 = "y "+ relations[rel] + (a*c)+"x<sup>2</sup> + "+((c*b)+(a*d))+"x + "+(b*d);
+
+  var tmp = "y "+ relations[rel] + "("+a+"x ";
+  if(b>0)
+  {
+    tmp += "+" + Math.abs(b) + ")("+c+"x ";
   }
   else
   {
-    return "y "+ relation() + (a*c)+"x<sup>2</sup> + "+((c*b)+(a*d))+"x + "+(b*d);
+    tmp += "&minus;" + Math.abs(b) + ")("+c+"x ";
   }
+
+  if(d>0)
+  {
+    tmp += "+" + Math.abs(d) + ")";
+  }
+  else
+  {
+    tmp += "&minus;" + Math.abs(d) + ")";
+  }
+
+  var form2 = tmp;
+
+  var answer = form1;
+  var other = form2;
+  if(Random())
+  {
+    answer = form2;
+    other = form1;
+  }
+
+  answer +="<br><div class='answer' name='answer'>"+other+"<br>";
+  answer +=a+"x"+(b>0?"+":"&minus;")+Math.abs(b)+"=0 "+c+"x"+(d>0?"+":"&minus;")+Math.abs(d)+"=0&nbsp;&nbsp;Test for y=0<br>";
+  answer +=a+"x="+(-b)+" "+c+"x="+(-d)+"<br>";
+  answer +="x="+(-b)+"/"+a+" x="+(-d)+"/"+c+"<br><br>";
+  answer +="y="+(a*c)+"(0)<sup>2</sup> + "+((c*b)+(a*d))+"(0) + "+(b*d)+"&nbsp;&nbsp;Test for x=0<br>";
+  answer +="y="+(b*d)+"<br><br>";
+  var xm = (-((c*b)+(a*d)))+"/"+(2*a*c)+")";
+  var xmval = (-((c*b)+(a*d)))/(2*a*c);
+  answer +="x<sub>m</sub>="+xm+"&nbsp;&nbsp;Find mid-point @ -b/2a<br>";
+  answer +="y<sub>m</sub>="+(a*c)+"("+xm+")<sup>2</sup> + "+((c*b)+(a*d))+"("+xm+") + "+(b*d)+"&nbsp;&nbsp;Find y<sub>m</sub><br>";
+  tmp = (a*c*xmval*xmval)+(((c*b)+(a*d))*xmval)+(b*d);
+  answer +="y<sub>m</sub>="+tmp;
+  answer += "</div>";
+
+  return answer;
 }
 
 function circle()
@@ -60,7 +110,17 @@ function circle()
   b = flip(Math.floor(Random()*5+1));
   r = Math.floor(Random()*5+1);
 
-  return "0 "+ relation() + " x<sup>2</sup> + y<sup>2</sup> + "+ (2*a) +"x + "+ (2*b) +"y + "+((a*a)+(b*b)-(r*r));
+  var rel = relation();
+  var tmp= "0 "+ rel + " x<sup>2</sup> + y<sup>2</sup> + "+ (2*a) +"x + "+ (2*b) +"y + "+((a*a)+(b*b)-(r*r));
+
+  var answer = tmp+"<br><div class='answer' name='answer'>";
+  answer += (-((a*a)+(b*b)-(r*r)))+ rel + " x<sup>2</sup> + "+ (2*a) +"x&nbsp;&nbsp;&nbsp;&nbsp;+ y<sup>2</sup> + "+ (2*b) +"y&nbsp;&nbsp;Reorder and leave a space</br>";
+  answer += (-((a*a)+(b*b)-(r*r)))+ rel + " (x + "+ a +")<sup>2</sup> + (y + "+ b +")<sup>2</sup>&nbsp;&nbsp;Complete square<br>";
+  answer += "<span>"+(a*a)+" + "+(b*b)+"</span>+"+(-((a*a)+(b*b)-(r*r)))+ rel + " x<sup>2</sup> + "+ (2*a) +"x + "+(a*a)+"+ y<sup>2</sup> + "+ (2*b) +"y + "+(b*b)+"&nbsp;&nbsp;Add third term to both sides<br>";
+  answer += r+"<sup>2</sup>"+ rel + " (x + "+ a +")<sup>2</sup> + (y + "+ b +")<sup>2</sup>&nbsp;&nbsp;Square root radius<br>";
+  answer += "</div>";
+
+  return answer;
 }
 
 function ellipse()
@@ -71,7 +131,20 @@ function ellipse()
   d = flip(Math.floor(Random()*5+1));
   r = Math.floor(Random()*5+1);
 
-  return "0 "+ relation() + " " + (d*d) +"x<sup>2</sup> + "+ (c*c)+"y<sup>2</sup> + "+ (2*a*d*d) +"x + "+ (2*b*c*c) +"y + "+((d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r));
+  var rel = relation();
+  var tmp = "0 "+ rel + " " + (d*d) +"x<sup>2</sup> + "+ (c*c)+"y<sup>2</sup> + "+ (2*a*d*d) +"x + "+ (2*b*c*c) +"y + "+((d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r));
+
+  var answer = tmp+"<br><div class='answer' name='answer'>";
+  answer += (-((d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r)))+ rel + " " + (d*d) +"x<sup>2</sup> + "+ (2*a*d*d) +"x + &nbsp;&nbsp;&nbsp;&nbsp;+ "+ (c*c)+"y<sup>2</sup> + "+(2*b*c*c) +"y&nbsp;&nbsp;Reorder and leave a space</br>";
+  answer += (-((d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r)))+ rel + " " + (d*d) +"(x<sup>2</sup> + "+ (2*a) +"x + &nbsp;&nbsp;&nbsp;&nbsp;)+ "+ (c*c)+"(y<sup>2</sup> + "+(2*b) +"y&nbsp;&nbsp;&nbsp;&nbsp;)&nbsp;&nbsp;Factor the leading coefficient</br>";
+  answer += (-((d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r)))+ rel + " " + (d*d) +"(x + "+ a +")<sup>2</sup>+ "+ (c*c)+"(y + "+b+")<sup>2</sup>&nbsp;&nbsp;Complete squares</br>";
+  answer += (-((d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r)))+ rel + " " + (d*d) +"(x<sup>2</sup> + "+ (2*a) +"x + "+(a*a)+")+ "+ (c*c)+"(y<sup>2</sup> + "+(2*b) +"y + "+(b*b)+")&nbsp;&nbsp;Fill in third term</br>";
+  answer += (a*a*d*d)+" + " +(b*b*c*c)+" + "+(-((d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r)))+ rel + " " + (d*d) +"x<sup>2</sup> + "+ (2*a*d*d) +"x + "+(a*a*d*d)+"+"+ (c*c)+"y<sup>2</sup> + "+(2*b*c*c) +"y + "+(b*b*c*c)+"&nbsp;&nbsp;Distribute and add on both sides</br>";
+  answer += (c*c*d*d*r*r)+"/["+(c*c)+"*"+(d*d)+"] "+ rel + " " + (d*d) +"(x + "+ a +")<sup>2</sup>/["+(c*c)+"*"+(d*d)+ "] + "+(c*c)+"(y + "+b+")<sup>2</sup>/["+(c*c)+"*"+(d*d)+"]&nbsp;&nbsp;Divide by both factors</br>";
+  answer += r+"<sup>2</sup>"+ rel + " (x + "+ a +")<sup>2</sup>/"+Math.abs(c)+"<sup>2</sup> + (y + "+b+")<sup>2</sup>/"+Math.abs(d)+"<sup>2</sup>&nbsp;&nbsp;Square root radius</br>";
+  answer += "</div>";
+
+  return answer;
 }
 
 function hyperbola()
@@ -82,14 +155,33 @@ function hyperbola()
   d = flip(Math.floor(Random()*5+1));
   r = 1;
 
+  var xCoeff = 1;
   if(Random()>.5)
   {
-    return "0 "+ relation() + " -" + (d*d) +"x<sup>2</sup> + "+ (c*c)+"y<sup>2</sup> + "+ (-2*a*d*d) +"x + "+ (2*b*c*c) +"y + "+(-(d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r));
+    xCoeff = -1;
   }
-  else
-  {
-    return "0 "+ relation() + " " + (d*d) +"x<sup>2</sup> - "+ (c*c)+"y<sup>2</sup> + "+ (2*a*d*d) +"x + "+ (-2*b*c*c) +"y + "+((d*d*a*a)-(c*c*b*b)-(c*c*d*d*r*r));
-  }
+  var rel = relation();
+  var end = xCoeff>0?((d*d*a*a)-(c*c*b*b)-(c*c*d*d*r*r)):(-(d*d*a*a)+(c*c*b*b)-(c*c*d*d*r*r));
+  temp = "0 "+ rel + (xCoeff>0?" +":" -") + (d*d) +"x<sup>2</sup>"+(xCoeff>0?" -":" +")+ (c*c)+"y<sup>2</sup> + "+ (2*a*d*d) +"x + "+ (-2*b*c*c) +"y + "+end;
+
+  var answer = temp+"<br><div class='answer' name='answer'>";
+  answer += (-end)+ rel + (xCoeff>0?" +":" -") + (d*d) +"x<sup>2</sup> "+(2*a*d*d) +"x"+"&nbsp;&nbsp;&nbsp;&nbsp;"+(xCoeff>0?" - ":" + ")+(c*c)+"y<sup>2</sup> + "+(-2*b*c*c) +"y&nbsp;&nbsp;Reorder and leave a space<br>";
+  var x2 = xCoeff>0?(2*a):(-2*a);
+  var y2 = xCoeff>0?(2*b):(-2*b);
+  answer += (-end)+ rel + (xCoeff>0?" +":" -") + (d*d) +"(x<sup>2</sup> +"+x2+"x&nbsp;&nbsp;&nbsp;&nbsp;)"+(xCoeff>0?" - ":" + ")+ (c*c)+"(y<sup>2</sup> + "+ y2 +"y&nbsp;&nbsp;&nbsp;&nbsp;)&nbsp;&nbsp;Factor leading coefficients<br>";
+  answer += (-end)+ rel + (xCoeff>0?" +":" -") + (d*d) +"(x +"+(x2/2)+")<sup>2</sup>"+(xCoeff>0?" - ":" + ")+ (c*c)+"(y + "+ (y2/2) +")<sup>2</sup>&nbsp;&nbsp;Complete the squares<br>";
+  answer += (-end)+ rel + (xCoeff>0?" +":" -") + (d*d) +"(x<sup>2</sup> +"+x2+"x + "+((x2/2)*(x2/2))+")"+(xCoeff>0?" - ":" + ")+ (c*c)+"(y<sup>2</sup> + "+ y2 +"y + "+((y2/2)*(y2/2))+")&nbsp;&nbsp;Replace missing third term<br>";
+  var x3 = xCoeff>0?(d*(x2/2)*(x2/2)*d):-(d*(x2/2)*(x2/2)*d);
+  var y3 = xCoeff>0?-(c*(y2/2)*(y2/2)*c):(c*(y2/2)*(y2/2)*c);
+  answer += x3+" + " +y3+" + " +(-end)+ rel + (xCoeff>0?" +":" -") + (d*d) +"x<sup>2</sup> "+(2*a*d*d) +"x + "+x3+(xCoeff>0?" - ":" + ")+(c*c)+"y<sup>2</sup> + "+(-2*b*c*c) +"y + "+y3+"&nbsp;&nbsp;Distribute and add on both sides<br>";
+  var rwf = x3+y3-end;
+  answer += rwf+"/["+(c*c)+"*"+(d*d)+"]" + rel + (xCoeff>0?" +":" -") + (d*d) +"(x +"+(x2/2)+")<sup>2</sup>/["+(d*d)+"*"+(c*c)+"]"+(xCoeff>0?" - ":" + ")+ (c*c)+"(y + "+ (y2/2) +")<sup>2</sup>/["+(c*c)+"*"+(d*d)+"]&nbsp;&nbsp;Complete the squares<br>";
+  answer += "1 " + rel + (xCoeff>0?" +":" -") +"(x +"+(x2/2)+")<sup>2</sup>/"+Math.abs(c)+"<sup>2</sup>"+(xCoeff>0?" - ":" + ")+ "(y + "+ (y2/2) +")<sup>2</sup>/"+Math.abs(d)+"<sup>2</sup>"+"&nbsp;&nbsp;Complete the squares<br>";
+
+
+  answer += "</div>";
+
+  return answer;
 }
 
 function ConicSections()
