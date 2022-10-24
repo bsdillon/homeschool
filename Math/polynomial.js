8f51afe4ce16577681986d84f5603d1c6575d1f4
bsdillon <57143227+bsdillon@users.noreply.github.com>
2022-10-24 14:18:06 -0400

    Update polynomial.js

--- Math/polynomial.js
+++ Math/polynomial.js
@@ -16,16 +16,30 @@ function factoredPolynomial()
   var s = "";
   for(let i=0;i<coeffs.length;i++)
   {
-    s += "(x "
-    if(i<=10)
+    if(coeffs[i]>0)
     {
-      s+= "+ ";
+      if(i<10)
+      {
+        s+= "(x + ";
+        s += (i-10)+")<sup>"+coeffs[i]+"</sup>";
+      }
+      else if(i==10)
+      {
+        if(coeffs[i]==0)
+        {
+          s= "x"+s;
+        }
+        else
+        {
+          s= "x<sup>"+coeffs[i]+"</sup>"+s;
+        }
+      }
+      else
+      {
+        s+= "(x &minus; ";
+        s += (i-10)+")<sup>"+coeffs[i]+"</sup>";
+      }
     }
-    else
-    {
-      s+= "&minus; ";
-    }
-    s += i+")<sup>"+coeffs[i]+"</sup>";
   }
   return s;
 }
